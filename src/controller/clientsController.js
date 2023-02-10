7134a3751e8e468cada87bf1d0c234fb43e219aa
dudamollica <dudarenno@yahoo.com.br>
2023-02-10 14:49:34 -0300

    Feat: Validando busca de cliente pelo id

--- src/controller/clientsController.js
+++ src/controller/clientsController.js
@@ -13,6 +13,7 @@ export async function showEspecificClient(req, res) {
   const { id } = req.params;
   try {
     const client = await db.query(`SELECT * FROM customers WHERE id=${id}`);
+    if (client.rows.length == 0) return res.status(404).send("Status 404");
     res.send(client.rows[0]);
   } catch (error) {
     res.status(500).send(error.message);
@@ -23,12 +24,12 @@ export async function insertClient(req, res) {
   const { name, phone, cpf, birthday } = req.body;
   try {
     const client = await db.query(`SELECT * FROM customers WHERE cpf='${cpf}'`);
-    if(client.rows.length>0) return res.status(409).send("Status 409")
+    if (client.rows.length > 0) return res.status(409).send("Status 409");
 
     await db.query(
       `INSERT INTO customers (name, phone, cpf, birthday) VALUES ('${name}','${phone}','${cpf}','${birthday}')`
     );
-    res.status(201).send("Ok")
+    res.status(201).send("Ok");
   } catch (error) {
     res.status(500).send(error.message);
   }
--- src/schemas/clientsSchema.js
+++ src/schemas/clientsSchema.js
@@ -3,6 +3,6 @@ import joi from "joi";
 export const clientsSchema = joi.object({
   name: joi.string().required(),
   phone: joi.string().min(10).max(11).required(),
-  cpf: joi.string().min(11).max(11).required(),
+  cpf: joi.string().regex(/^[0-9]+$/).length(11).required(),
   birthday: joi.date().required(),
 });
