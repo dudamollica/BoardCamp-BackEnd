bb5c82971593c3d60c57be882bc345ad6a31202f
dudamollica <dudarenno@yahoo.com.br>
2023-02-11 18:21:26 -0300

    Feat: Inserindo Aluguel

--- src/controller/rentsController.js
+++ src/controller/rentsController.js
@@ -1,4 +1,5 @@
 import db from "../config/database.js";
+import dayjs from "dayjs";
 
 export async function showRents(req, res) {
   try {
@@ -8,6 +9,21 @@ export async function showRents(req, res) {
     res.status(500).send(error.message);
   }
 }
-export async function insertRent(req, res) {}
+export async function insertRent(req, res) {
+  const { customerId, gameId, daysRented } = req.body;
+  try {
+    let price = await db.query(`SELECT ("pricePerDay") FROM games WHERE id=${gameId}`)
+    price = price.rows[0].pricePerDay
+    await db.query(
+      `INSERT INTO rentals 
+      ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
+      VALUES (${customerId}, ${gameId}, '${dayjs().format("YYYY-MM-DD")}', ${daysRented}, null, ${price*daysRented}, null)`
+    );
+    res.status(201).send("Ok");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
 export async function finalizeRent(req, res) {}
 export async function deleteRent(req, res) {}
--- src/routes/rentsRoutes.js
+++ src/routes/rentsRoutes.js
@@ -5,11 +5,14 @@ import {
     deleteRent,
   } from "../controller/rentsController.js";
   import { Router } from "express";
+  import { validateSchema } from "../middleware/validateSchema.js";
+  import { rentsSchema } from "../schemas/rentsSchema.js";
+  import validateClientGame from "../middleware/validateClientGame.js";
   
   const rentsRouter = Router();
   
   rentsRouter.get("/rentals", showRents);
-  rentsRouter.post("/rentals", insertRent);
+  rentsRouter.post("/rentals", validateSchema(rentsSchema), validateClientGame, insertRent);
   rentsRouter.post("/rentals/:id/return", finalizeRent);
   rentsRouter.get("/rentals/:id", deleteRent);
   
